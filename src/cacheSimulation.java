5d02823ed201fc2c7df9cd8a33e94694941c3ab5
bevzxcs <97271510+bevzxcs@users.noreply.github.com>
2023-11-28 00:37:00 +0800

    Update and rename cacheSimulator.java to cacheSimulation.java

---
+++ src/cacheSimulation.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CacheSimulation {
+    // Constants
+    private static final int NUM_CACHE_BLOCKS = 32;
+    private static final int CACHE_LINE_SIZE = 64;
+    private static final int NUM_SETS = 8;
+    private static final int WAYS_PER_SET = 8;
+
+    // Cache data structure
+    private String[][] cacheData;
+
+    // GUI components
+    private JTextField memoryBlocksTextField;
+    private JRadioButton stepByStepRadioButton;
+    private JRadioButton finalMemoryRadioButton;
+    private Table cacheTable;
+
+    // Constructor
+    public CacheSimulation(JTextField memoryBlocksTextField, JRadioButton stepByStepRadioButton,
+                           JRadioButton finalMemoryRadioButton, Table cacheTable) {
+        this.memoryBlocksTextField = memoryBlocksTextField;
+        this.stepByStepRadioButton = stepByStepRadioButton;
+        this.finalMemoryRadioButton = finalMemoryRadioButton;
+        this.cacheTable = cacheTable;
+
+        // Initialize cache data structure
+        cacheData = new String[NUM_CACHE_BLOCKS][CACHE_LINE_SIZE + 2];
+        initializeCache();
+
+        // Set up action listener for the "Simulate" button
+        simulateButtonAction();
+    }
+
+    // Initialize cache with empty data
+    private void initializeCache() {
+        for (int i = 0; i < NUM_CACHE_BLOCKS; i++) {
+            for (int j = 0; j < CACHE_LINE_SIZE + 2; j++) {
+                cacheData[i][j] = "";
+            }
+        }
+    }
+
+    // Simulate cache with 8-way BSA + MRU
+    private void simulateCache(int numMemoryBlocks) {
+        // TODO: Implement cache simulation logic based on the specified mapping and replacement policy
+        // Use the provided number of memory blocks and update the cache data structure accordingly
+
+        // For demonstration purposes, let's just mark some cache lines as "hit" for now
+        markCacheLineAsHit(0, 0);
+        markCacheLineAsHit(2, 1);
+        markCacheLineAsHit(4, 2);
+    }
+
+    // Mark a cache line as "hit" in the cache data structure
+    private void markCacheLineAsHit(int block, int set) {
+        cacheData[block * WAYS_PER_SET + set][CACHE_LINE_SIZE + 1] = "Hit";
+        // TODO: Update other relevant information in the cache data structure
+    }
+
+    // Action listener for the "Simulate" button
+    private void simulateButtonAction() {
+        JButton simulateButton = new JButton("Simulate");
+        simulateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get the number of memory blocks from the user input
+                int numMemoryBlocks = Integer.parseInt(memoryBlocksTextField.getText());
+
+                // Simulate the cache based on the selected options
+                if (stepByStepRadioButton.isSelected()) {
+                    // TODO: Implement step-by-step tracing (animation)
+                } else if (finalMemoryRadioButton.isSelected()) {
+                    // Simulate the cache and update the GUI with the final memory snapshot
+                    simulateCache(numMemoryBlocks);
+                    updateCacheTable();
+                }
+            }
+        });
+    }
+
+    // Update the cache table in the GUI with the simulated data
+    private void updateCacheTable() {
+        // TODO: Implement the logic to update the cache table in the GUI with the simulated data
+        // You can use the cacheData array to update the JTable
+        // For demonstration purposes, let's just update the GUI with the current cacheData
+        cacheTable.updateTable(cacheData);
+    }
+}
