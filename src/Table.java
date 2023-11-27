bddb4bc3ae0601225b0744f7ac1945e90efb0c88
Thea <127085937+thea1210@users.noreply.github.com>
2023-11-28 00:48:10 +0800

    With Final Snapshot

--- src/Table.java
+++ src/Table.java
@@ -8,55 +8,19 @@ public class Table {
 	JTable j;
 
 	// Constructor
-	JPanel panel(String title)
+	JPanel panel(String title, String[][] data)
 	{
 		// Frame initialization
 		JPanel f = new JPanel();
         JLabel label2 = new JLabel(title);
         f.add(label2);
 
-		// Data to be displayed in the JTable
-		String[][] data = {
-			{ "0", "0", "" },
-			{ "0", "1", "" },
-            { "0", "2", "" },
-            { "0", "3", "" },
-            { "0", "4", "" },
-            { "0", "5", "" },
-            { "0", "6", "" },
-            { "0", "7", "" },
-            { "1", "0", "" },
-			{ "1", "1", "" },
-            { "1", "2", "" },
-            { "1", "3", "" },
-            { "1", "4", "" },
-            { "1", "5", "" },
-            { "1", "6", "" },
-            { "1", "7", "" },
-            { "2", "0", "" },
-			{ "2", "1", "" },
-            { "2", "2", "" },
-            { "2", "3", "" },
-            { "2", "4", "" },
-            { "2", "5", "" },
-            { "2", "6", "" },
-            { "2", "7", "" },
-            { "3", "0", "" },
-			{ "3", "1", "" },
-            { "3", "2", "" },
-            { "3", "3", "" },
-            { "3", "4", "" },
-            { "3", "5", "" },
-            { "3", "6", "" },
-            { "3", "7", "" },
-		};
-
 		// Column Names
 		String[] columnNames = { "Block", "Block Set", "Data" };
 
 		// Initializing the JTable
 		j = new JTable(data, columnNames);
-		j.setSize(200, 300);
+		j.setSize(200, 800);
 
 
 		f.add(j);
