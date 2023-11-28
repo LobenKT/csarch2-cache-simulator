da69340d9ba6ec30b1285ace96bc5096191dca04
Thea1210 <althea_arceta@dlsu.edu.ph>
2023-11-28 19:26:26 +0800

    Added Generate Text Log Button

--- src/App.java
+++ src/App.java
@@ -277,50 +277,6 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1500, 800);
 
-        // Creating the panel at bottom and adding components
-        JPanel outerpanel = new JPanel(); 
-        JPanel panel = new JPanel(); 
-        JPanel bpanel = new JPanel();
-        JPanel panel2 = new JPanel();
-        JLabel label = new JLabel("Number of Memory Blocks:");
-        JTextField tf = new JTextField(5);
-        tf.setText("5");
-        JLabel label1 = new JLabel("Inputs:");
-        JTextField tf1 = new JTextField(30);
-        JLabel label2 = new JLabel("Option:");
-        JRadioButton j1 = new JRadioButton("Step-by-Step Tracing");
-        JRadioButton j2 = new JRadioButton("Final Memory Snapshot", true);
-        j1.setOpaque(false);
-        j2.setOpaque(false);
-        ButtonGroup rd = new ButtonGroup();
-        rd.add(j1);
-        rd.add(j2);
-        JButton send = new JButton("Simulate");
-
-        panel.add(label);
-        panel.add(tf);
-        panel.add(label1);
-        panel.add(tf1);
-        panel.setSize(1500, 100);
-
-        bpanel.add(label2);
-        bpanel.add(j1);
-        bpanel.add(j2);
-        bpanel.setSize(1500, 100);
-
-        panel2.add(send);
-        panel2.setSize(1500, 100);
-
-        panel.setBackground(Color.LIGHT_GRAY);
-        bpanel.setBackground(Color.LIGHT_GRAY);
-        panel2.setBackground(Color.LIGHT_GRAY);
-
-        outerpanel.add(panel);
-        outerpanel.add(bpanel);
-        outerpanel.add(panel2);
-        outerpanel.setLayout(new BoxLayout(outerpanel, BoxLayout.Y_AXIS));
-        outerpanel.setBackground(Color.PINK);
-
         // Simulation Part
         JPanel simuPanel = new JPanel();
         Table cache = new Table();
@@ -347,10 +303,15 @@ public class App {
         textpanel.add(missrate_label);
         textpanel.add(avetime_label);
         textpanel.add(totaltime_label);
+
+        JButton textlog = new JButton("Generate Text Log");
+        textpanel.add(textlog);
+
         textpanel.setLayout(new BoxLayout(textpanel, BoxLayout.Y_AXIS));
         
         simuPanel.add(cache.panel("Cache Memory", data));
         simuPanel.add(textpanel);
+
 
         // Adding Components to the frame
         frame.setLayout(new BorderLayout());
@@ -358,26 +319,10 @@ public class App {
         frame.add(BorderLayout.NORTH, simuPanel);
         
         // When simulate is clicked
-        send.addActionListener((ActionListener) new ActionListener(){
+        textlog.addActionListener((ActionListener) new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                int n = Integer.valueOf(tf.getText());
-                String inputValue = tf1.getText().toString();
-                String[] testcase = inputValue.split("\\s");
-                ArrayList<String> inputarr = new ArrayList<String>( Arrays.asList(testcase));
-                if (testcase.length < n*2){ //if input are less than
-                    for (int i = testcase.length; i < n*2; i++) {
-                        inputarr.add(null);
-                    }
-                }
-                
-                if(j1.isSelected()){
-                    // TODO : Step-by-Step Tracing
-
-                } else if(j2.isSelected()){
-                    //Final Snapshot
-                    fSnap(default_data, inputarr, n);
-                }
+                //insert code
             }
         });
         frame.setVisible(true);
