d1e473c676c6c6bee75fa17b092c623717738ef5
Jose Tristan T. Tan <50431327+Shiray427@users.noreply.github.com>
2023-11-29 20:31:49 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -4,28 +4,28 @@ n = 32
 passed = False
 
 while not passed:
-    choice = int(input("Select test case to generate (1-3): "))
-    passed = True
-    match choice:
-        case 1:
-            for i in range(4):
-                for j in range(2*n):
-                    print(j)
-        case 2:
-            for i in range(n*4):
-                print(randint(0, n*4))
-        case 3:
-            sequence = []
-            for i in range(4):
-                for j in range(n-1):
-                    sequence.append(j)
-                for j in range(1, 2*n):
-                    sequence.append(j)
-            for ele in sequence:
-                print(ele)
-        case _:
-            passed = False
-            print("Please input a proper choice.")
-        
-                
-    
+    try:
+        choice = int(input("Select test case to generate (1-3): "))
+        passed = True
+        match choice:
+            case 1:
+                for i in range(4):
+                    for j in range(2*n):
+                        print(j)
+            case 2:
+                for i in range(n*4):
+                    print(randint(0, n*4))
+            case 3:
+                sequence = []
+                for i in range(4):
+                    for j in range(n-1):
+                        sequence.append(j)
+                    for j in range(1, 2*n):
+                        sequence.append(j)
+                for ele in sequence:
+                    print(ele)
+            case _:
+                passed = False
+                print("Please input a proper choice.")
+    except:
+        print("Please input a proper choice.")
